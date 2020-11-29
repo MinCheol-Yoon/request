cdba75b62db2a4b3b7018ec7ac58c6048b2e1cae
MinChoel-Yoon <yoon56@g.skku.edu>
2020-11-29 22:07:52 +0900

    add aa.c file and edit a.c file

--- a.c
+++ a.c
@@ -3,5 +3,8 @@
 int main(){
     int a = 5;
     int b = 3;
+    int d = 4;
+    int f = 98;
+    printf("a+d = %d", (a+d));
     return 0;
 }
---
+++ aa.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int kor, eng, math;
+	int sum = 0;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+	sum = 80 + 100 + 90;
+
+	printf("This program print report card.\n");
+
+	printf("Korean: %d\n", kor);
+	pirntf("English: %d\n": eng);
+	printf("Math: %d\n", math);
+	printf("sum: %d\n", sum);
+
+}
